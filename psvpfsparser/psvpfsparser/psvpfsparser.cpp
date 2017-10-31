dcd9fd5081c1eb3c59645ca16e2df8fb8394d775
motoharu-gosuto <motoharu.gosuto@gmail.com>
2017-10-31 23:56:06 +0300

    use in place indexing in match_by_size

--- psvpfsparser/psvpfsparser/psvpfsparser.cpp
+++ psvpfsparser/psvpfsparser/psvpfsparser.cpp
@@ -9,24 +9,35 @@
 #include "UnicvDbParser.h"
 #include "FilesDbParser.h"
 
-int match_by_size(const scei_rodb_t& unicv, const std::vector<sce_ng_pfs_file_t>& files)
+int match_by_size(const scei_rodb_t& unicv_arg, const std::vector<sce_ng_pfs_file_t>& files_arg)
 {
-   std::vector<std::pair<uint32_t, scei_ftbl_t>> unicv_files_by_block_size;
-   std::vector<std::pair<uint32_t, sce_ng_pfs_file_t>> filesdb_files_by_block_size;
-   
+   std::vector<std::pair<uint32_t, std::pair<uint32_t,scei_ftbl_t>>> unicv_files_by_block_size;
+   std::vector<std::pair<uint32_t, std::pair<uint32_t,sce_ng_pfs_file_t>>> filesdb_files_by_block_size;
+
+   std::vector<std::pair<uint32_t, sce_ng_pfs_file_t>> files;
+
+   int i = 0;
+   for(auto& f : files_arg)
+      files.push_back(std::make_pair(i++, f));
+
+   std::vector<std::pair<uint32_t, scei_ftbl_t>> tables;
+
+   for(auto& t : unicv_arg.tables)
+      tables.push_back(std::make_pair(i++, t));
+
    for(auto& f : files)
    {
-      if(f.file.global_index >= unicv.tables.size())
+      if(f.first >= tables.size())
       {
          std::cout << "Index out of range" << std::endl;
          return -1;
       }
 
-      const scei_ftbl_t& tbl = unicv.tables.at(f.file.global_index);
-      uint32_t nSectors = tbl.ftHeader.nSectors;
+      auto& tbl = tables.at(f.first);
+      uint32_t nSectors = tbl.second.ftHeader.nSectors;
 
-      uint32_t nBlocks = f.file.info.size / tbl.ftHeader.fileDbSectorSize;
-      uint32_t nTail = f.file.info.size % tbl.ftHeader.fileDbSectorSize;
+      uint32_t nBlocks = f.second.file.info.size / tbl.second.ftHeader.fileDbSectorSize;
+      uint32_t nTail = f.second.file.info.size % tbl.second.ftHeader.fileDbSectorSize;
       nTail = nTail > 0 ? 1 : 0;
       nBlocks = nBlocks + nTail;
 
@@ -34,12 +45,12 @@ int match_by_size(const scei_rodb_t& unicv, const std::vector<sce_ng_pfs_file_t>
       filesdb_files_by_block_size.push_back(std::make_pair(nBlocks, f));
 
       std::sort(unicv_files_by_block_size.begin(), unicv_files_by_block_size.end(), 
-         [](const std::pair<uint32_t, scei_ftbl_t> &left, const std::pair<uint32_t, scei_ftbl_t> &right) {
+         [](const std::pair<uint32_t, std::pair<uint32_t, scei_ftbl_t>> &left, const std::pair<uint32_t, std::pair<uint32_t, scei_ftbl_t>> &right) {
             return left.first > right.first;
       });
 
       std::sort(filesdb_files_by_block_size.begin(), filesdb_files_by_block_size.end(), 
-         [](const std::pair<uint32_t, sce_ng_pfs_file_t> &left, const std::pair<uint32_t, sce_ng_pfs_file_t> &right) {
+         [](const std::pair<uint32_t, std::pair<uint32_t, sce_ng_pfs_file_t>> &left, const std::pair<uint32_t, std::pair<uint32_t, sce_ng_pfs_file_t>> &right) {
             return left.first > right.first;
       });
    }
