22f53deff66324104d1d26f7fa1b619116102eb8
motoharu-gosuto <motoharu.gosuto@gmail.com>
2017-11-02 23:48:36 +0300

    add caching to f00d decryptor

--- psvpfsparser/psvpfsparser/F00DKeyEncryptor.cpp
+++ psvpfsparser/psvpfsparser/F00DKeyEncryptor.cpp
@@ -10,6 +10,13 @@
 
 #include "Utils.h"
 
+static F00DKeyEncryptor g_F00D_encryptor;
+
+F00DKeyEncryptor* get_F00D_encryptor()
+{
+   return &g_F00D_encryptor;
+}
+
 std::string F00DKeyEncryptor::create_url(unsigned const char* key, int key_size)
 {
    std::stringstream ss;
@@ -77,6 +84,23 @@ int F00DKeyEncryptor::execute_url(std::string url)
    return -1;
 }
 
+int F00DKeyEncryptor::parse_key_base(unsigned const char* key, unsigned char* dest, int key_size, std::string jkey, std::string jdrv_key)
+{
+   int nbytes = key_size / 8;
+
+   unsigned char key_check[0x20];
+   string_to_byte_array(jkey, nbytes, key_check);
+   
+   if(memcmp(key, key_check, nbytes) != 0)
+      return -1;
+
+   string_to_byte_array(jdrv_key, nbytes, dest);
+
+   m_keyCache.insert(std::make_pair(jkey, jdrv_key));
+
+   return 0;
+}
+
 int F00DKeyEncryptor::parse_key(unsigned const char* key, unsigned char* dest, int key_size)
 {
    std::string json(m_response);
@@ -91,30 +115,34 @@ int F00DKeyEncryptor::parse_key(unsigned const char* key, unsigned char* dest, i
    std::string jkey = pt.get_child("key").data();
    std::string jdrv_key = pt.get_child("drv_key").data();
 
-   int nbytes = key_size / 8;
-
-   unsigned char key_check[0x20];
-   string_to_byte_array(jkey, nbytes, key_check);
-   
-   if(memcmp(key, key_check, nbytes) != 0)
-      return -1;
-
-   string_to_byte_array(jdrv_key, nbytes, dest);
-
-   return 0;
+   return parse_key_base(key, dest, key_size, jkey, jdrv_key);
 }
 
 int F00DKeyEncryptor::encrypt_key(unsigned const char* key, int key_size, unsigned char* drv_key)
 {
-   if(key_size != 0x80 && key_size != 0xC0 && key_size != 0x100)
+   if(key_size != 0x80 && 
+      // key_size != 0xC0 && //TODO: need to implement padding
+      key_size != 0x100)
       return -1;
 
-   std::string url = create_url(key, key_size);
-   if(execute_url(url) < 0)
-      return -1;
+   std::string keyStr = byte_array_to_string(key, key_size / 8);
 
-   if(parse_key(key, drv_key, key_size) < 0)
-      return -1;
+   auto kit = m_keyCache.find(keyStr);
+   if(kit != m_keyCache.end())
+   {
+      int nbytes = key_size / 8;
+      string_to_byte_array(kit->second, nbytes, drv_key);
+      return 0;
+   }
+   else
+   {
+      std::string url = create_url(key, key_size);
+      if(execute_url(url) < 0)
+         return -1;
 
-   return 0;
+      if(parse_key(key, drv_key, key_size) < 0)
+         return -1;
+
+      return 0;
+   }
 }
--- psvpfsparser/psvpfsparser/F00DKeyEncryptor.h
+++ psvpfsparser/psvpfsparser/F00DKeyEncryptor.h
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <stdint.h>
+
 #include <string>
+#include <map>
 
 class F00DKeyEncryptor
 {
 private:
    std::string m_response;
+
+   std::map<std::string, std::string> m_keyCache;
 
 private:
    std::string create_url(unsigned const char* key, int key_size);
@@ -16,8 +21,11 @@ private:
    int execute_url(std::string url);
 
 private:
+   int parse_key_base(unsigned const char* key, unsigned char* dest, int key_size, std::string jkey, std::string jdrv_key);
    int parse_key(unsigned const char* key, unsigned char* dest, int key_size);
 
 public:
    int encrypt_key(const unsigned char* key, int key_size, unsigned char* drv_key);
 };
+
+F00DKeyEncryptor* get_F00D_encryptor();
