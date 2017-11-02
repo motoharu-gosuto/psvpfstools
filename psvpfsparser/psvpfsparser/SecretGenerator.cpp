aa1fedca8e0716bc48f1b4ec3414cb8ddbdf6562
motoharu-gosuto <motoharu.gosuto@gmail.com>
2017-11-03 02:03:22 +0300

    remove irrelevant wrappers from secret generator

--- psvpfsparser/psvpfsparser/SecretGenerator.cpp
+++ psvpfsparser/psvpfsparser/SecretGenerator.cpp
@@ -10,19 +10,6 @@
 #include "SceSblSsMgrForDriver.h"
 #include "SceKernelUtilsForDriver.h"
 
-//TODO: this is only part of the reversed code
-//TODO: Crypto engine consists of multiple layers, where each layer should be placed into separate file
-
-int sha1Digest(unsigned char *result, const unsigned char *source, int size)
-{
-   return SceKernelUtilsForDriver_sceSha1DigestForDriver(source, size, result);
-}
-
-int hmacSha1Digest(unsigned char* digest, const unsigned char* key, const unsigned char* data, int data_len)
-{
-   return SceKernelUtilsForDriver_sceHmacSha1DigestForDriver(key, 0x14, data, data_len, digest);
-}
-
 int AESCBCEncryptWithKeygen_base(const unsigned char* klicensee, unsigned char* iv, uint32_t size, const unsigned char* src, unsigned char* dst, uint16_t key_id)
 {
    uint16_t kid = 0 - (key_id - 1) + (key_id - 1); // ???
@@ -73,13 +60,13 @@ int gen_secret(unsigned char* combo_aligned, uint32_t salt0, uint32_t salt1)
    if(salt0 == 0)
    {
       saltin0[0] = salt1;
-      hmacSha1Digest(base, hmac_key1, (unsigned char*)saltin0, 4); // derive base with one salt
+      SceKernelUtilsForDriver_sceHmacSha1DigestForDriver(hmac_key1, 0x14, (unsigned char*)saltin0, 4, base); // derive base with one salt
    }
    else
    {
       saltin1[0] = salt0;
       saltin1[1] = salt1;
-      hmacSha1Digest(base, hmac_key1, (unsigned char*)saltin1, 8); // derive base with two salts
+      SceKernelUtilsForDriver_sceHmacSha1DigestForDriver(hmac_key1, 0x14, (unsigned char*)saltin1, 8, base); // derive base with two salts
    }
 
    memcpy(combo_aligned, base, 0x14); // calculated digest will be src data
@@ -124,23 +111,22 @@ int generate_secret(unsigned char* secret, const unsigned char* klicensee,  uint
    unsigned char combo[0x28] = {0};
    unsigned char drvkey[0x14] = {0};
 
-   sha1Digest(base0, klicensee, 0x10); // calculate digest of klicensee
-
+   SceKernelUtilsForDriver_sceSha1DigestForDriver(klicensee, 0x10, base0); // calculate digest of klicensee
+   
    saltin[0] = 0xA;
    saltin[1] = salt1;
 
-   sha1Digest(base1, (unsigned char*)saltin, 8); // calculate digest of salt
+   SceKernelUtilsForDriver_sceSha1DigestForDriver((unsigned char*)saltin, 8, base1); // calculate digest of salt
 
    memcpy(combo, base0, 0x14);
    memcpy(combo + 0x14, base1, 0x14);
          
-   sha1Digest(drvkey, combo, 0x28); // calculate digest of combination of klicensee and salt digests
+   SceKernelUtilsForDriver_sceSha1DigestForDriver(combo, 0x28, drvkey); // calculate digest of combination of klicensee and salt digests
                
    memcpy(secret, drvkey, 0x14); // copy derived key
 
    return 0;
 }
-
 
 int scePfsUtilGetSecret(unsigned char* secret, const unsigned char* klicensee, uint32_t salt0, uint16_t flag, uint32_t salt1, uint16_t key_id)
 {
