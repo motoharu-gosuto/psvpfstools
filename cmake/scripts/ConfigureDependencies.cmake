ffc0fb70493ff00a56c6c91bf270921319442997
motoharu-gosuto <motoharu.gosuto@gmail.com>
2017-11-04 16:12:45 +0300

    add curl to configuration add gitignore

--- cmake/scripts/ConfigureDependencies.cmake
+++ cmake/scripts/ConfigureDependencies.cmake
@@ -24,3 +24,26 @@ message("Boost library is not found")
 endif()
 
 endfunction(configure_boost)
+
+#environment variables for configure_curl have to be set
+#CURL_INCLUDE_DIR
+#CURL_LIBRARY
+
+function(configure_curl)
+
+message("configuring curl")
+
+set(CURL_INCLUDE_DIR "$ENV{CURL_INCLUDE_DIR}")
+set(CURL_LIBRARY "$ENV{CURL_LIBRARY}")
+
+FIND_PACKAGE(CURL REQUIRED)
+
+if(CURL_FOUND)
+message("Using CURL_VERSION_STRING: ${CURL_VERSION_STRING}")
+message("Using CURL_INCLUDE_DIRS: ${CURL_INCLUDE_DIRS}")
+message("Using CURL_LIBRARIES: ${CURL_LIBRARIES}")
+else()
+message("Curl library is not found")
+endif()
+
+endfunction(configure_curl)
