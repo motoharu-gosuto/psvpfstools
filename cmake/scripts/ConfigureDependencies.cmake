c4b173dc467771d040c123d153bd484421cdd7ac
motoharu-gosuto <motoharu.gosuto@gmail.com>
2017-11-04 15:37:39 +0300

    add cmake files

---
+++ cmake/scripts/ConfigureDependencies.cmake
@@ -0,0 +1,26 @@
+#environment variables for configure_boost have to be set
+#BOOST_INCLUDEDIR
+#BOOST_LIBRARYDIR
+#BOOST_ROOT
+
+function(configure_boost)
+
+message("configuring boost")
+
+set (Boost_USE_STATIC_LIBS ON)
+set (Boost_USE_MULTITHREADED ON)
+
+set (BOOST_COMPONENTS system 
+                      filesystem)
+
+FIND_PACKAGE(Boost COMPONENTS ${BOOST_COMPONENTS} REQUIRED)
+
+if(Boost_FOUND)
+message("Using Boost_VERSION: ${Boost_VERSION}")
+message("Using Boost_INCLUDE_DIRS: ${Boost_INCLUDE_DIRS}")
+message("Using Boost_LIBRARY_DIRS: ${Boost_LIBRARY_DIRS}")
+else()
+message("Boost library is not found")
+endif()
+
+endfunction(configure_boost)
