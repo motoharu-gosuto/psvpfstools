df17000d7f6941538fcba6239ca7e7fe2e719f85
motoharu-gosuto <motoharu.gosuto@gmail.com>
2017-12-06 23:48:36 +0300

    fix main file

--- psvpfsparser/psvpfsparser.cpp
+++ psvpfsparser/psvpfsparser.cpp
@@ -75,7 +75,7 @@ int execute(PsvPfsParserConfig& cfg)
    if(parseFilesDb(klicensee, titleIdPath, header, files, dirs) < 0)
       return -1;
 
-   scei_rodb_t unicv;
+   std::shared_ptr<scei_db_base_t> unicv;
    if(parseUnicvDb(titleIdPath, unicv) < 0)
       return -1;
 
