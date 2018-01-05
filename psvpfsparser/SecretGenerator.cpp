eaad943f9e41c65b6e3e69cfc1d78e7fcb6d748e
motoharu-gosuto <motoharu.gosuto@gmail.com>
2018-01-05 18:34:49 +0300

    move some common code from SecretGenerator to IcvPrimitives

--- psvpfsparser/SecretGenerator.cpp
+++ psvpfsparser/SecretGenerator.cpp
@@ -7,7 +7,8 @@
 #include <libcrypto/sha1.h>
 
 #include "PfsKeys.h"
-#include "SceKernelUtilsForDriver.h"
+#include "IcvPrimitives.h"
+
 #include "PfsCryptEngineBase.h"
 #include "FilesDbParser.h"
 
@@ -20,14 +21,14 @@ int gen_secret(unsigned char* combo_aligned, std::uint32_t files_salt, std::uint
    {
       int saltin0[1] = {0};
       saltin0[0] = unicv_page_salt;
-      SceKernelUtilsForDriver_sceHmacSha1DigestForDriver(hmac_key1, 0x14, (unsigned char*)saltin0, 4, base); // derive base with one salt
+      icv_set_hmac_sw(base, hmac_key1, (unsigned char*)saltin0, 4); // derive base with one salt
    }
    else
    {
       int saltin1[2] = {0};
       saltin1[0] = files_salt;
       saltin1[1] = unicv_page_salt;
-      SceKernelUtilsForDriver_sceHmacSha1DigestForDriver(hmac_key1, 0x14, (unsigned char*)saltin1, 8, base); // derive base with two salts
+      icv_set_hmac_sw(base, hmac_key1, (unsigned char*)saltin1, 8); // derive base with two salts
    }
 
    memcpy(combo_aligned, base, 0x14); // calculated digest will be src data
@@ -59,20 +60,16 @@ int generate_secret(unsigned char* secret, const unsigned char* klicensee,  std:
    int saltin[2] = {0};
    unsigned char base0[0x14] = {0};
    unsigned char base1[0x14] = {0};
-   unsigned char combo[0x28] = {0};
    unsigned char drvkey[0x14] = {0};
 
-   SceKernelUtilsForDriver_sceSha1DigestForDriver(klicensee, 0x10, base0); // calculate digest of klicensee
-   
+   icv_set_sw(base0, klicensee, 0x10); // calculate digest of klicensee
+
    saltin[0] = 0xA;
    saltin[1] = unicv_page_salt;
 
-   SceKernelUtilsForDriver_sceSha1DigestForDriver((unsigned char*)saltin, 8, base1); // calculate digest of salt
+   icv_set_sw(base1, (unsigned char*)saltin, 8); // calculate digest of salt
 
-   memcpy(combo, base0, 0x14);
-   memcpy(combo + 0x14, base1, 0x14);
-         
-   SceKernelUtilsForDriver_sceSha1DigestForDriver(combo, 0x28, drvkey); // calculate digest of combination of klicensee and salt digests
+   icv_contract(drvkey, base0, base1); // calculate digest of combination of klicensee and salt digests
                
    memcpy(secret, drvkey, 0x14); // copy derived key
 
