6f57afc00da637c9f4b3aabe8dc4ecff7844ed29
motoharu-gosuto <motoharu.gosuto@gmail.com>
2018-01-12 01:56:14 +0300

    add get_isUnicv function

--- psvpfsparser/UnicvDbParser.cpp
+++ psvpfsparser/UnicvDbParser.cpp
@@ -11,6 +11,35 @@
 
 #include "UnicvDbTypes.h"
 
+int get_isUnicv(boost::filesystem::path titleIdPath, bool& isUnicv)
+{
+   boost::filesystem::path root(titleIdPath);
+
+   boost::filesystem::path filepath = root / "sce_pfs" / "unicv.db";
+
+   if(!boost::filesystem::exists(filepath))
+   {
+      boost::filesystem::path filepath2 = root / "sce_pfs" / "icv.db";
+      if(!boost::filesystem::exists(filepath2) || !boost::filesystem::is_directory(filepath2))
+      {
+         std::cout << "failed to find unicv.db file or icv.db folder" << std::endl;
+
+         isUnicv = false;
+         return -1;
+      }
+      else
+      {
+         isUnicv = false;
+         return 0;
+      }
+   }
+   else
+   {
+      isUnicv = true;
+      return 0;
+   }
+}
+
 int parseUnicvDb(boost::filesystem::path titleIdPath, std::shared_ptr<sce_idb_base_t>& fdb)
 {
    boost::filesystem::path root(titleIdPath);
--- psvpfsparser/UnicvDbParser.h
+++ psvpfsparser/UnicvDbParser.h
@@ -2,4 +2,6 @@
 
 #include "UnicvDbTypes.h"
 
+int get_isUnicv(boost::filesystem::path titleIdPath, bool& isUnicv);
+
 int parseUnicvDb(boost::filesystem::path titleIdPath, std::shared_ptr<sce_idb_base_t>& fdb);
