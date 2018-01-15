3e163174fb9d2141e3cd417a7326548b921ffc70
motoharu-gosuto <motoharu.gosuto@gmail.com>
2018-01-15 03:06:34 +0300

    retest crypt engine. fix tweak_key derivation bug introduced during refactoring

--- psvpfsparser/PfsCryptEngine.cpp
+++ psvpfsparser/PfsCryptEngine.cpp
@@ -11,6 +11,7 @@
 
 //----------------------
 
+//[TESTED both branches]
 void verify_step(CryptEngineWorkCtx* crypt_ctx, std::uint16_t mode_index, unsigned char* source)
 {
    //check that it is not a directory and does have icv
@@ -24,8 +25,6 @@ void verify_step(CryptEngineWorkCtx* crypt_ctx, std::uint16_t mode_index, unsign
 
    if(!is_gamedata(mode_index))
    {
-      throw std::runtime_error("Untested branch in verify_step");
-
       if((crypt_ctx->subctx->data->pmi_bcl_flag & (PMI_BCL_CRYPTO_USE_CMAC | PMI_BCL_SKIP_DECRYPT)) != (PMI_BCL_CRYPTO_USE_CMAC | PMI_BCL_SKIP_DECRYPT))
       {
          if(crypt_ctx->subctx->nBlocks != 0)
@@ -62,8 +61,6 @@ void verify_step(CryptEngineWorkCtx* crypt_ctx, std::uint16_t mode_index, unsign
    }
    else
    {
-      throw std::runtime_error("Untested branch in verify_step");
-
       if((crypt_ctx->subctx->data->pmi_bcl_flag & (PMI_BCL_CRYPTO_USE_CMAC | PMI_BCL_SKIP_DECRYPT)) != (PMI_BCL_CRYPTO_USE_CMAC | PMI_BCL_SKIP_DECRYPT))
       {
          std::uint32_t tweak_key = crypt_ctx->subctx->sector_base;
@@ -109,6 +106,7 @@ void verify_step(CryptEngineWorkCtx* crypt_ctx, std::uint16_t mode_index, unsign
    }
 }
 
+//[TESTED both branches]
 void work_3_step0(CryptEngineWorkCtx* crypt_ctx, std::uint16_t mode_index, unsigned char* buffer)
 {
    //check that it is not a directory and is encrypted
@@ -140,12 +138,10 @@ void work_3_step0(CryptEngineWorkCtx* crypt_ctx, std::uint16_t mode_index, unsig
    int offset = 0;
    std::uint32_t counter = 0;
 
-   std::uint64_t tweak_key = crypt_ctx->subctx->data->block_size + crypt_ctx->subctx->sector_base;
+   std::uint64_t tweak_key = crypt_ctx->subctx->data->block_size * crypt_ctx->subctx->sector_base;
 
    if(!is_gamedata(mode_index))
    {
-      throw std::runtime_error("Untested branch in work_3_step0");
-
       do
       {
          pfs_decrypt_icv(key, tweak_enc_key, 0x80, tweak_key + offset, crypt_ctx->subctx->data->block_size, crypt_ctx->subctx->data->block_size, buffer + offset, buffer + offset, crypt_ctx->subctx->data->pmi_bcl_flag);
@@ -157,8 +153,6 @@ void work_3_step0(CryptEngineWorkCtx* crypt_ctx, std::uint16_t mode_index, unsig
    }
    else
    {
-      throw std::runtime_error("Untested branch in work_3_step0");
-
       std::uint32_t bytes_left = crypt_ctx->subctx->data->block_size * (crypt_ctx->subctx->nBlocks - 1) + (crypt_ctx->subctx->tail_size);
    
       do
