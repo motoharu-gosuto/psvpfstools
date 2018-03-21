3303b84509972f9b316eb46ccb70be7b61129345
motoharu-gosuto <motoharu.gosuto@gmail.com>
2018-03-22 00:22:58 +0300

    minor type adjustments to SceKernelUtilsForDriver

--- psvpfsparser/SceKernelUtilsForDriver.cpp
+++ psvpfsparser/SceKernelUtilsForDriver.cpp
@@ -13,13 +13,13 @@ int SceKernelUtilsForDriver_aes_encrypt_2(aes_context* ctx, const unsigned char*
 
 //##### SW HASH FUNCTIONS #####
 
-int SceKernelUtilsForDriver_sceSha1DigestForDriver(const unsigned char *source, std::uint32_t size, unsigned char result[0x14])
+int SceKernelUtilsForDriver_sceSha1DigestForDriver(const unsigned char *source, int size, unsigned char result[0x14])
 {
    sha1(source, size, result);
    return 0;
 }
 
-int SceKernelUtilsForDriver_sceHmacSha1DigestForDriver(const unsigned char* key, std::uint32_t key_len, const unsigned char* data, std::uint32_t data_len, unsigned char digest[0x14])
+int SceKernelUtilsForDriver_sceHmacSha1DigestForDriver(const unsigned char* key, int key_len, const unsigned char* data, int data_len, unsigned char digest[0x14])
 {
    sha1_hmac(key, key_len, data, data_len, digest);
    return 0;
--- psvpfsparser/SceKernelUtilsForDriver.h
+++ psvpfsparser/SceKernelUtilsForDriver.h
@@ -3,6 +3,6 @@
 #include <cstdint>
 #include <libcrypto/aes.h>
 
-int SceKernelUtilsForDriver_sceSha1DigestForDriver(const unsigned char *source, std::uint32_t size, unsigned char result[0x14]);
+int SceKernelUtilsForDriver_sceSha1DigestForDriver(const unsigned char *source, int size, unsigned char result[0x14]);
 
-int SceKernelUtilsForDriver_sceHmacSha1DigestForDriver(const unsigned char* key, std::uint32_t key_len, const unsigned char* data, std::uint32_t data_len, unsigned char digest[0x14]);
+int SceKernelUtilsForDriver_sceHmacSha1DigestForDriver(const unsigned char* key, int key_len, const unsigned char* data, int data_len, unsigned char digest[0x14]);
