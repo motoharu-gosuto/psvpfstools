f8708bc42caa5b0486941f3c0f7a0f7ee2609c5f
motoharu-gosuto <motoharu.gosuto@gmail.com>
2018-03-23 01:31:49 +0300

    extend ICryptoOperations interface

--- psvpfsparser/ICryptoOperations.h
+++ psvpfsparser/ICryptoOperations.h
@@ -9,6 +9,9 @@ public:
    virtual int aes_cbc_encrypt(const unsigned char* src, unsigned char* dst, int size, const unsigned char* key, int key_size, unsigned char* iv) const = 0;
    virtual int aes_cbc_decrypt(const unsigned char* src, unsigned char* dst, int size, const unsigned char* key, int key_size, unsigned char* iv) const = 0;
    
+   virtual int aes_ctr_encrypt(const unsigned char* src, unsigned char* dst, int size, const unsigned char* key, int key_size, unsigned char* iv) = 0;
+   virtual int aes_ctr_decrypt(const unsigned char* src, unsigned char* dst, int size, const unsigned char* key, int key_size, unsigned char* iv) = 0;
+
    virtual int aes_ecb_encrypt(const unsigned char* src, unsigned char* dst, int size, const unsigned char* key, int key_size) const = 0;
    virtual int aes_ecb_decrypt(const unsigned char* src, unsigned char* dst, int size, const unsigned char* key, int key_size) const = 0;
    
